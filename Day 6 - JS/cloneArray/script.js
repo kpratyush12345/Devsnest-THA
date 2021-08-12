9f0fad0e0dbf23f3e8aa31c06c3bf893b728a0f3
Pratyush Kumar <66811279+kpratyush12345@users.noreply.github.com>
2021-08-12 19:02:56 +0530

    Delete Day 6 - JS directory

--- Day 6 - JS/cloneArray/script.js
+++
@@ -1,9 +0,0 @@
-"use script";
-
-// Write a JavaScript function to clone an array\
-function array_Clone(obj) {
-  return [...obj]; //using spread Operator
-}
-
-console.log(array_Clone([1, 2, 4, 0]));
-console.log(array_Clone([1, 2, [4, 0]]));
--- Day 6 - JS/isArray/script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-// Write a JavaScript function to check whether an `input` is an array or not
-function is_array(obj) {
-  return Array.isArray(obj);
-}
-
-console.log(is_array([1, 2, 4, 0]));
-console.log(is_array("w3resource"));
